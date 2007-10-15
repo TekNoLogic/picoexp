d5db11b2dc0c8abaa4e78b9d5346aea4f5fdf43e
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-10-15 03:38:15 +0000

    Importing EXPBlock
    git-svn-id: http://tekkub-wow.googlecode.com/svn/trunk/EXPBlock@639 86fe6d9a-1522-0410-a387-bf9db416f0a0

---
+++ EXPBlock.lua
@@ -0,0 +1,95 @@
+
+
+------------------------------
+--      Are you local?      --
+------------------------------
+
+local start, max, starttime, startlevel
+
+
+-------------------------------------------
+--      Namespace and all that shit      --
+-------------------------------------------
+
+EXPBlock = DongleStub("Dongle-1.0"):New("EXPBlock")
+local lego = DongleStub("LegoBlock-Beta0"):New("EXPBlock", "99%", "Interface\\Addons\\EXPBlock\\icon")
+
+
+---------------------------
+--      Init/Enable      --
+---------------------------
+
+function EXPBlock:Initialize()
+	local blockdefaults = {
+		locked = false,
+		showIcon = false,
+		showText = true,
+		shown = true,
+		noresize = true,
+		width = 46,
+	}
+
+	self.db = self:InitializeDB("EXPBlockDB", {profile = {block = blockdefaults}}, "global")
+end
+
+
+function EXPBlock:Enable()
+	lego:SetDB(self.db.profile.block)
+
+	start, max, starttime = UnitXP("player"), UnitXPMax("player"), GetTime()
+	startlevel = UnitLevel("player") + start/max
+
+	self:RegisterEvent("PLAYER_XP_UPDATE")
+	self:RegisterEvent("PLAYER_LEVEL_UP")
+
+	self:PLAYER_XP_UPDATE()
+end
+
+
+------------------------------
+--      Event Handlers      --
+------------------------------
+
+function EXPBlock:PLAYER_XP_UPDATE()
+	lego:SetText(string.format("%d%%", UnitXP("player")/UnitXPMax("player")*100))
+end
+
+
+function EXPBlock:PLAYER_LEVEL_UP()
+	start = start - max
+	max = UnitXPMax("player")
+end
+
+
+------------------------
+--      Tooltip!      --
+------------------------
+
+local function GetTipAnchor(frame)
+	local x,y = frame:GetCenter()
+	if not x or not y then return "TOPLEFT", "BOTTOMLEFT" end
+	local hhalf = (x > UIParent:GetWidth()/2) and "RIGHT" or "LEFT"
+	local vhalf = (y > UIParent:GetHeight()/2) and "TOP" or "BOTTOM"
+	return vhalf..hhalf, frame, (vhalf == "TOP" and "BOTTOM" or "TOP")..hhalf
+end
+
+
+lego:SetScript("OnLeave", function() GameTooltip:Hide() end)
+lego:SetScript("OnEnter", function(self)
+ 	GameTooltip:SetOwner(self, "ANCHOR_NONE")
+	GameTooltip:SetPoint(GetTipAnchor(self))
+	GameTooltip:ClearLines()
+
+	GameTooltip:AddLine("EXPBlock")
+
+	local cur = UnitXP("player")
+
+	GameTooltip:AddDoubleLine("EXP:", cur.."/"..max)
+	GameTooltip:AddDoubleLine("TNL:", max-cur)
+	GameTooltip:AddLine(string.format("%.1f hours played this session", (GetTime()-starttime)/3600))
+	GameTooltip:AddLine((cur - start).." EXP gained this session")
+	GameTooltip:AddLine(string.format("%.1f levels gained this session", UnitLevel("player") + cur/max - startlevel))
+
+	GameTooltip:Show()
+end)
+
