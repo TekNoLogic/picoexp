0220bf08a618738a1914a3cc20d6f61abdbbf214
Tekkub <tekkub@gmail.com>
2009-06-13 15:30:49 -0600

    More decimal places!

--- picoEXP.lua
+++ picoEXP.lua
@@ -76,9 +76,9 @@ function dataobj.OnEnter(self)
 	GameTooltip:AddDoubleLine("EXP:", cur.."/"..max, nil,nil,nil, 1,1,1)
 	GameTooltip:AddDoubleLine("Rest:", string.format("%d%%", (GetXPExhaustion() or 0)/max*100), nil,nil,nil, 1,1,1)
 	GameTooltip:AddDoubleLine("TNL:", max-cur, nil,nil,nil, 1,1,1)
-	GameTooltip:AddLine(string.format("%.1f hours played this session", (GetTime()-starttime)/3600), 1,1,1)
+	GameTooltip:AddLine(string.format("%.2f hours played this session", (GetTime()-starttime)/3600), 1,1,1)
 	GameTooltip:AddLine((cur - start).." EXP gained this session", 1,1,1)
-	GameTooltip:AddLine(string.format("%.1f levels gained this session", UnitLevel("player") + cur/max - startlevel), 1,1,1)
+	GameTooltip:AddLine(string.format("%.2f levels gained this session", UnitLevel("player") + cur/max - startlevel), 1,1,1)
 
 	GameTooltip:Show()
 end
